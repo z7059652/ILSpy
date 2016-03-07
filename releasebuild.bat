235cc35ffe6fea0cfa4b2ed93d295ec08f32771e
z7059652 <7059652@163.com>
2016-03-07 16:11:08 +0800

    first

---
+++ releasebuild.bat
@@ -0,0 +1,9 @@
+@if not exist "AvalonEdit\ICSharpCode.AvalonEdit.sln" (
+	git submodule update --init || exit /b 1
+)
+%windir%\microsoft.net\framework\v4.0.30319\msbuild /m ILSpy.sln /p:Configuration=Release "/p:Platform=Any CPU"
+@IF %ERRORLEVEL% NEQ 0 GOTO err
+@exit /B 0
+:err
+@PAUSE
+@exit /B 1
